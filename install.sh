e2e5bed1f2868f17c235f039f0a53c76d239dc21
cc <iz4iz4@users.noreply.github.com>
2021-06-30 16:05:22 +0800

    update custom web.tar.gz

--- install.sh
+++ install.sh
@@ -439,7 +439,7 @@ function generate_certificate() {
 function configure_web() {
   rm -rf /www/xray_web
   mkdir -p /www/xray_web
-  wget -O web.tar.gz https://raw.githubusercontent.com/wulabing/Xray_onekey/main/basic/web.tar.gz
+  wget -O web.tar.gz https://raw.githubusercontent.com/cc-404/Xray_onekey/main/basic/web.tar.gz
   tar xzf web.tar.gz -C /www/xray_web
   judge "站点伪装"
   rm -f web.tar.gz
